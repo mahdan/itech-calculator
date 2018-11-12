03692b8b4e1876cb0692018731861236d5f1d095
Mandip Ahdan <mandip.ahdan@gmail.com>
2018-11-12 17:08:05 +0000

    check to see if its a number or operator pressed

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -4,6 +4,23 @@ const keys = calculator.querySelector('.calculator__keys')
 // Add event listeners for key presses
 keys.addEventListener('click', e => {
   if (e.target.matches('button')) {
-    // Do something
+    // determing the type of key pressed with a data-action attribute
+    const key = e.target
+    const action = key.dataset.action
+
+    // check if it doesnt have data-action attribute
+    if (!action) {
+      console.log('number key!')
+    }
+
+    // check if it does have data-action attribute
+    if (
+      action === 'add' ||
+      action === 'subtract' ||
+      action === 'multiply' ||
+      action === 'divide'
+    ) {
+      console.log('operator key!')
+    }
   }
 })
